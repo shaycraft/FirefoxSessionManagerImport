b5df33556ab436601c63f2f80ae33e5a316d0613
Sam <phreakdawg@gmail.com>
2017-11-25 14:45:04 -0700

    Disposing properly of connection

--- api/server.js
+++ api/server.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var app = express();
 
-var handleError = function(err, res) {
+var handleError = function (err, res) {
     console.log('ERROR in server.js...');
     console.log(err);
     res.send(err);
 }
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('sup bruh');
 });
 
-app.get('/tabs', function(req, res) {
-   var repo = require('../import/repository'); 
-   repo.getTabs()
-   .then(data => {
-       res.send(data.rows);
-       repo.dispose();       
-   }, err => handleError(err, res));
+app.get('/tabs', async function (req, res) {
+    var repo = require('../import/repository');
+    await repo.init();
+    repo.getTabs()
+        .then(data => {
+            res.send(data.rows);
+            repo.dispose();
+        }, err => handleError(err, res));
 });
 
 app.listen(3000);
