d77dbebbcc884d395b91ec82bfb9b5839fadb908
Sam Haycraft <phreakdawg@gmail.com>
2017-12-03 08:15:02 +0000

    Allowing cors

--- api/server.js
+++ api/server.js
@@ -16,6 +16,15 @@ var handleError = function (err, res) {
     res.send(err);
 }
 
+// allow cors
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', function (req, res) {
     res.send('sup bruh');
 });
