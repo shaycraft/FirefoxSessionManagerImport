3e9f0fb44338382e4e3165c3bcbb41e63ac93722
Sam Haycraft <shaycraft@metlife.com>
2017-12-14 15:38:58 -0700

    Gets auth header, but doesn't work, think a subject is needed

--- webapp/src/app/tabsession/tabsession.service.ts
+++ webapp/src/app/tabsession/tabsession.service.ts
@@ -8,22 +8,43 @@ import { Tabs } from '../models/tab.model';
 @Injectable()
 export class TabsessionService {
   private readonly BASE_URL: string = 'http://ffsmi.samhaycraft.net';
+  private _headers: any;
 
   constructor(private http: Http) {
-    console.log('DEBUG< in service constructor');
-    this.http.post(`${this.BASE_URL}/authorize`, { password: 'PASSWORD' })
-      .map(res => {
-        console.log('DEBUG, authorize called, result = ');
-        console.log(res);
-      })
-      .first()
-      .subscribe(data => {
-        console.log(data);
-      })
+    console.log('DEBUG, in service constructor');
+
   }
 
-  public getTabs(): Observable<Tabs[]> {
-    return this.http.get(`${this.BASE_URL}/tabs`)
+  private getToken(): Observable<string> {
+    return this.http.post(`${this.BASE_URL}/authorize`, { password: 'PASSWORD' })
+      .map(res => res.text());
+    /*
+      console.log('DEBUG, authorize called, result = ');
+      this._headers = [{ 'x-auth-token': res.text() }];
+      console.log(res.text());
+    })
+    .first()
+    .subscribe();*/
+  }
+
+  public getTabs(): Observable<Tabs[] {
+    if (this._headers) {
+      return this.getTabsCall();
+    }
+    else {
+       this.getToken()
+        .first()
+        .subscribe(x => {
+          this._headers = [{'x-auth-token': x }];
+          return this.getTabsCall();
+        });
+    }
+  }
+
+  private getTabsCall(): Observable<Tabs[]> {
+    console.log('Debug, headers = ');
+    console.log(this._headers);
+    return this.http.get(`${this.BASE_URL}/tabs`, { headers: this._headers })
       .map((res) => res.json());
   }
 }
